beadff0f53e3a519c4945892bdfd147b2f96b0f5
Peter <peterchampion1997@gmail.com>
2019-11-17 22:54:35 +0000

    Initial work on Camera & Character movement
    - Implemented start of a CameraManager class that will be intended to be a singleton class that handles all camera behaviour such as following the player character, updating based on player input, etc.
    - Started work on InputHandler class, which will record user input and pass this along to the attached StateManager below.
    - Started work on StateManager class that is intended to track all the current state info and perform state transitions of the assigned character.

---
+++ FinalGame/Assets/Scripts/InputHandler.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IsThisDarkSouls
+{
+    public class InputHandler : MonoBehaviour
+    {
+        private float vertical;
+        private float horizontal;
+        private StateManager states;
+        CameraManager cameraManager;
+        private float delta;
+
+        void Start()
+        {
+            states = GetComponent<StateManager>();
+            states.Initialise();
+            cameraManager = CameraManager.instance;
+            cameraManager.Initialse(this.transform);
+        }
+
+        private void Update()
+        {
+            delta = Time.deltaTime;
+            states.Tick(delta);
+        }
+
+        void FixedUpdate()
+        {
+            delta = Time.fixedDeltaTime;
+            GetInput();
+            UpdateStates();
+            states.FixedTick(delta);
+            cameraManager.Tick(delta);
+        }
+
+        private void GetInput()
+        {
+            vertical = Input.GetAxis("Vertical");
+            horizontal = Input.GetAxis("Horizontal");
+        }
+
+        /// <summary>
+        /// Updates
+        /// </summary>
+        private void UpdateStates()
+        {
+            Vector3 v = vertical * cameraManager.transform.forward;
+            Vector3 h = horizontal * cameraManager.transform.right;
+            states.movementDirection = (v + h).normalized; // Return normalized vector
+            float desiredMovement = Mathf.Abs(horizontal) + Mathf.Abs(vertical); // Add both values together
+            states.moveAmount = Mathf.Clamp01(desiredMovement); // Clamp between 0-1 and update StateManager 
+        }
+    }
+}
+
---
+++ FinalGame/Assets/Scripts/StateManager.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace IsThisDarkSouls
+{
+    public class StateManager : MonoBehaviour
+    {
+        #region Variables
+        public float moveAmount;
+        public Vector3 movementDirection;
+
+        [SerializeField] private float moveSpeed = 4;
+        [SerializeField] private float rotateSpeed = 5;
+
+        public bool grounded;
+
+        public GameObject activeModel;
+        [HideInInspector] public Animator charAnim;
+        [HideInInspector] public Rigidbody rigidBody;
+        [HideInInspector] public float delta;
+        [HideInInspector] public LayerMask ignoredLayers;
+        #endregion
+
+        /// <summary>
+        /// Performs set up references for variables needed for class methods.
+        /// </summary>
+        public void Initialise()
+        {
+            SetUpAnimator();
+            rigidBody = GetComponent<Rigidbody>();
+            rigidBody.angularDrag = 999;
+            rigidBody.drag = 4;
+            rigidBody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+            gameObject.layer = 8; // Set to player layer
+            ignoredLayers = ~(1 << 8); // Ignore layers 1 to 8
+        }
+
+        /// <summary>
+        /// Gains reference to attached Animator component and gameobject, disabled root motion.
+        /// </summary>
+        private void SetUpAnimator()
+        {
+            if (activeModel == null)
+            {
+                charAnim = GetComponent<Animator>();
+
+                if (charAnim == null)
+                {
+                    Debug.Log("No model found for " + gameObject.name);
+                }
+                else
+                {
+                    activeModel = charAnim.gameObject;
+                }
+            }
+
+            if (charAnim == null)
+            {
+                charAnim = activeModel.GetComponent<Animator>();
+            }
+
+            charAnim.applyRootMotion = false;
+        }
+
+        /// <summary>
+        /// Ran alongside Update, records deltaTime, performs ground check.
+        /// </summary>
+        public void Tick(float deltaTime)
+        {
+            delta = deltaTime;
+            grounded = IsGrounded();
+            charAnim.SetBool("isGrounded", grounded);
+        }
+
+        /// <summary>
+        /// Ran alongside FixedUpdate, records fixedDeltaTime, changes rigidbody drag based on movement and grounded status, applies rotation to the player character and movement animations.
+        /// </summary>
+        public void FixedTick(float fixedDeltaTime)
+        {
+            delta = fixedDeltaTime;
+
+            if (moveAmount > 0 || !grounded) // If there is any movement or if the player is not grounded...
+            {
+                rigidBody.drag = 0;
+            }
+            else
+            {
+                rigidBody.drag = 4;
+            }
+            
+            if (grounded)
+            {
+                rigidBody.velocity = movementDirection * (moveSpeed * moveAmount); // Apply force in the direction the player is heading
+            }
+
+            Vector3 targetDirection = movementDirection;
+            targetDirection.y = 0;
+
+            if (targetDirection == Vector3.zero)
+            {
+                targetDirection = transform.forward;
+            }
+
+            Quaternion targetRotation = Quaternion.LookRotation(targetDirection); // Calculate the rotation desired
+            Quaternion lerpedRotation = Quaternion.Slerp(transform.rotation, targetRotation, delta * moveAmount * rotateSpeed); // Lerp between current rotation and desired rotation
+            transform.rotation = lerpedRotation; // Apply lerped rotation
+
+            HandleMovementAnimations();
+        }
+
+        /// <summary>
+        /// Updates animator movement variables.
+        /// </summary>
+        private void HandleMovementAnimations()
+        {
+            charAnim.SetFloat("vertical", moveAmount, 0.1f, delta);
+        }
+
+        /// <summary>
+        /// Performs a ground check from the bottom centre of the player, checking if there is collision with any layers.
+        /// </summary>
+        public bool IsGrounded()
+        {
+            bool grounded = false;
+
+            Vector3 origin = transform.position + (Vector3.up * 0.5f);
+            Vector3 direction = Vector3.down;
+            float distance = (0.5f + 0.3f);
+
+            RaycastHit hit;
+
+            Debug.DrawRay(origin, direction * distance);
+
+            if (Physics.Raycast(origin, direction, out hit, distance, ignoredLayers))
+            {
+                grounded = true;
+
+                Vector3 targetPosition = hit.point;
+                transform.position = targetPosition;
+            }
+            return grounded;
+        }
+    }
+}
+
+
