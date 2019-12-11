31b8b925028da43f11d17d50cbeb1c5e630ad213
RosieMable <sciarrillo.nella@gmail.com>
2019-12-11 16:09:24 +0000

    Spirit System Started
    Can create friendly scriptable objects for spirits,
    UI is correctly displaying what spirit we have equipped
    UI manager started

---
+++ FinalGame/Assets/Scripts/Spirits_System/SpiritSystem.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using IsThisDarkSouls;
+
+[RequireComponent(typeof(StateManager))]
+public class SpiritSystem : MonoBehaviour
+{
+    [SerializeField]
+    private Spirit_ScriptableObj spirit;
+
+    private StateManager PlayerCharacter;
+
+    delegate void UIChecks(Spirit_ScriptableObj _spirit);
+    UIChecks UIDelegate;
+
+
+    private void OnEnable()
+    {
+        UIDelegate += UIManager.Instance.ManageSpiritUI;
+    }
+
+    //for now it is going to be in update, but once the inventory system is on, this should happen when "equipping" the spirit
+    private void Start()
+    {
+
+        OnEquipSpirit(spirit);
+    }
+
+    protected void ActiveAbility()
+    {
+        //Active Ability logic
+        AnimationLogic();
+    }
+
+    protected void AnimationLogic()
+    {
+        //AnimationLogic for the active ability
+    }
+
+    protected void PassiveAbility(Spirit_ScriptableObj _EquippedSpirit)
+    {
+        if (PlayerCharacter)
+        {
+            PlayerCharacter.health = PlayerCharacter.health + _EquippedSpirit.HealthModifier;
+            print(PlayerCharacter.health);
+        }
+        else
+        {
+            throw new System.Exception("No PlayerCharacter found!");
+        }
+    }
+
+    void OnEquipSpirit(Spirit_ScriptableObj _SpiritToEquip)
+    {
+        PlayerCharacter = gameObject.GetComponent<StateManager>();
+
+        if (_SpiritToEquip != null)
+        {
+            PassiveAbility(_SpiritToEquip);
+
+            //Update UI
+            UIDelegate?.Invoke(_SpiritToEquip);
+
+        }
+        else
+        {
+            //Update UI
+            UIDelegate?.Invoke(_SpiritToEquip);
+        }
+
+
+    }
+
+    private void OnDisable()
+    {
+     //   UIDelegate -= UIManager.Instance.ManageSpiritUI;
+    }
+}
---
+++ FinalGame/Assets/Scripts/UIManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : Singleton<UIManager>
+{
+    private static UIManager _instance;
+
+
+    #region "Spirit UI Variables"
+
+    public Image SpiritIcon;
+
+    public Image SpiritBar;
+
+    #endregion
+
+    public void ManageSpiritUI(Spirit_ScriptableObj _spiritToDisplay)
+    {
+        if (_spiritToDisplay!=null)
+        {
+            SpiritIcon.gameObject.SetActive(true);
+            SpiritBar.gameObject.SetActive(true);
+
+            SpiritIcon.sprite = _spiritToDisplay._spiritSprite;
+        }
+        else
+        {
+            SpiritIcon.gameObject.SetActive(false);
+
+            SpiritBar.gameObject.SetActive(false);
+        }
+    }
+}
