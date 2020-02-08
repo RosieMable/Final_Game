9196ad3529514b9397fcbdacbaeb1cd59a5e6924
Peter <peterchampion1997@gmail.com>
2020-02-08 18:50:03 +0000

    AmbientAudio class
    - Plays audio when the player enters within range, also selects a clip from random to play.

---
+++ FinalGame/Assets/Scripts/Audio/AmbientAudio.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace ZaldensGambit
+{
+    public class AmbientAudio : MonoBehaviour
+    {
+        private AudioSource audioSource;
+        private StateManager player;
+        [SerializeField] private AudioClip[] audioClips;
+        private bool enteredRange;
+
+        private void Start()
+        {
+            audioSource.clip = audioClips[0];
+        }
+
+        private void Update()
+        {
+            float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+
+            if (distanceToPlayer < audioSource.minDistance && !enteredRange)
+            {
+                enteredRange = true;
+                int clipToPlay = Random.Range(0, audioClips.Length - 1);
+                audioSource.clip = audioClips[clipToPlay];
+                audioSource.Play();
+            }
+            else if (distanceToPlayer > audioSource.maxDistance)
+            {
+                enteredRange = false;
+                audioSource.Stop();
+            }
+
+            if (!audioSource.isPlaying && enteredRange)
+            {
+                int clipToPlay = Random.Range(0, audioClips.Length - 1);
+                audioSource.clip = audioClips[clipToPlay];
+                audioSource.Play();
+            }
+        }
+    }
+}
