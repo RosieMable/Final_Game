6046aecc98e6d8262c97f403ddd68422b144e996
Peter <peterchampion1997@gmail.com>
2020-03-15 02:37:04 +0000

    Initial Card Deck / Inventory system
    - Created a central class to store information on the spirit & dungeon cards that the player currently owns.
    - Supports adding/removing dungeon & spirit cards.
    - Has a limit of spirit/dungeon cards that can be carried which can be set in the inspector.
    - Has delegates which are intended to be used later when an inventory UI is added.

---
+++ FinalGame/Assets/Scripts/Cards System/CardInventory.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardInventory : MonoBehaviour
+{
+    public CardInventory instance;
+    [SerializeField] private int dungeonCardLimit = 50;
+    [SerializeField] private int spiritCardLimit = 20;
+
+    public List<Spirit_ScriptableObj> spiritCards;
+    public List<Card_ScriptableObj> dungeonCards;
+    public delegate void SpiritDeckUpdated(Spirit_ScriptableObj spirit);
+    public SpiritDeckUpdated spiritDeckCallback;
+    public delegate void DungeonDeckUpdated(Card_ScriptableObj dungeonCard);
+    public DungeonDeckUpdated dungeonDeckCallback;
+
+    private void Awake()
+    {
+        // Singleton setup
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(this);
+        }
+    }
+
+    private void Start()
+    {
+        foreach (Spirit_ScriptableObj spirit in spiritCards)
+        {
+            print("Spirit in inventory: " + spirit.name);
+        }
+
+        foreach (Card_ScriptableObj dungeonCard in dungeonCards)
+        {
+            print("Dungeon card in inventory: " + dungeonCard.name);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            RemoveSpiritCardFromInventory(spiritCards[0]);
+            RemoveDungeonCardFromInventory(dungeonCards[0]);
+        }
+    }
+
+    /// <summary>
+    /// Attempts to add a spirit to the list of spirits stored.
+    /// </summary>
+    public bool AddSpiritCardToInventory(Spirit_ScriptableObj spirit)
+    {
+        if (spiritCards.Count + 1 <= spiritCardLimit)
+        {
+            spiritCards.Add(spirit);
+            spiritDeckCallback.Invoke(spirit); // Invoke delegate passing the spirit added to the inventory
+            return true;
+        }
+
+        print("Spirit could not be added to the deck, too many cards!");
+        return false;
+    }
+
+    /// <summary>
+    /// Attempts to add a dungeon card to the list of dungeon cards stored.
+    /// </summary>
+    public bool AddDungeonCardToInventory(Card_ScriptableObj dungeonCard)
+    {
+        if (dungeonCards.Count + 1 <= dungeonCardLimit)
+        {
+            dungeonCards.Add(dungeonCard);
+            dungeonDeckCallback.Invoke(dungeonCard); // Invoke delegate passing the dungeonCard added to the inventory
+            return true;
+        }
+
+        print("Dungeon Card could not be added to the deck, too many cards!");
+        return false;
+    }
+
+    /// <summary>
+    /// Remove the chosen spirit from the inventory.
+    /// </summary>
+    public void RemoveSpiritCardFromInventory(Spirit_ScriptableObj spirit)
+    {
+        spiritCards.Remove(spirit);
+        print(spirit.name + " removed from inventory");
+    }
+
+    /// <summary>
+    /// Remove the chosen dungeon card from the inventory.
+    /// </summary>
+    public void RemoveDungeonCardFromInventory(Card_ScriptableObj dungeonCard)
+    {
+        dungeonCards.Remove(dungeonCard);
+        print(dungeonCard.name + " removed from inventory");
+    }
+
+    /// <summary>
+    /// Checks if the size of either list/deck exceeds their intended limit.
+    /// </summary>
+    private void CheckDeckSize()
+    {
+        if (spiritCards.Count > spiritCardLimit)
+        {
+            // TODO: Prompt removal of spirit cards
+        }
+
+        if (dungeonCards.Count > dungeonCardLimit)
+        {
+            // TODO: Prompt removal of dungeon cards
+        }
+    }
+}
