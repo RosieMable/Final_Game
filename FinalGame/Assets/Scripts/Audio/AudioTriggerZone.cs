71057e6a24631212a78d31be311d4a6ef3e2a5a0
Peter <peterchampion1997@gmail.com>
2020-04-30 14:18:38 +0100

    Updated AudioTrigger to not repeat

--- FinalGame/Assets/Scripts/Audio/AudioTriggerZone.cs
+++ FinalGame/Assets/Scripts/Audio/AudioTriggerZone.cs
@@ -8,13 +8,15 @@ namespace ZaldensGambit
     public class AudioTriggerZone : MonoBehaviour
     {
         [SerializeField] private AudioClip audioClip;
+        private bool played;
 
         private void OnTriggerEnter(Collider other)
         {
-            if (other.GetComponent<StateManager>() && audioClip != null)
+            if (other.GetComponent<StateManager>() && audioClip != null && played != true)
             {
                 CameraManager.instance.GetComponentInChildren<AudioSource>().clip = audioClip;
                 CameraManager.instance.GetComponentInChildren<AudioSource>().Play();
+                played = true;
             }
         }
     }
