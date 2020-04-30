c8d85f06696246b112b22f1415407ae750e4b45f
Peter <peterchampion1997@gmail.com>
2020-04-30 14:17:26 +0100

    AudioTrigger implemented + Outdated Player Prefab removed

---
+++ FinalGame/Assets/Scripts/Audio/AudioTriggerZone.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace ZaldensGambit
+{
+    public class AudioTriggerZone : MonoBehaviour
+    {
+        [SerializeField] private AudioClip audioClip;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.GetComponent<StateManager>() && audioClip != null)
+            {
+                CameraManager.instance.GetComponentInChildren<AudioSource>().clip = audioClip;
+                CameraManager.instance.GetComponentInChildren<AudioSource>().Play();
+            }
+        }
+    }
+}
